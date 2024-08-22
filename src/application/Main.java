3b6ba9510bce8623ee68e2b62e7290df8208bdbd
Juliana Girotto <ads.jjgirotto@gmail.com>
2024-08-22 16:07:42 -0300

    Department class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,38 +1,12 @@
 package application;
 
-import db.DB;
-import db.DbIntegrityException;
+import model.entities.Department;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = null;
-        PreparedStatement st = null;
-        try {
-            conn = DB.getConnection();
-
-            st = conn.prepareStatement(
-                    "DELETE FROM department"
-                    + "WHERE"
-                    + "Id = ?"
-            );
-
-            st.setInt(1, 5);
-
-            int rowsAffected = st.executeUpdate();
-
-            System.out.println("Done! Rows affected: " + rowsAffected);
-        }
-        catch (SQLException e) {
-            throw new DbIntegrityException(e.getMessage());
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-        }
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
     }
 }
